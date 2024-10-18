19fa7025cb9712f2372de93bd7a284c1c2a8b1c9
Julien Coulombe <jcoulombemorency@gmail.com>
2024-10-18 08:51:34 -0400

    feat: started implementing smf/shmf calculators. need to update the application to use the newly added functions in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,25 +5,26 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const convertWeightToLbs = (
-  weight: number,
-  weightPreference: string
-): string => {
+export const convertWeightToLbs = (weight: number, weightPreference: string): string => {
   if (weightPreference === "lbs") {
     return (weight * 2.20462).toFixed(2);
   }
   return weight.toFixed(2);
 };
 
-export const convertWeightToKg = (
-  weight: number,
-  weightPreference: string
-): string => {
+export const convertWeightToKg = (weight: number, weightPreference: string): string => {
   if (weightPreference === "lbs") {
     return (weight / 2.20462).toFixed(2);
   }
   return weight.toFixed(2);
 };
 
-export const clamp = (val: number, min: number, max: number): number =>
-  Math.min(Math.max(val, min), max);
+export const computeSinclairCoefficient = (bodyWeight: number, A: number, referenceBodyWeight: number): number => {
+  return 10 ** (A * Math.log10(bodyWeight / referenceBodyWeight) ** 2);
+};
+
+export const computeStandardizedTotal = (total: number, sinclair: number, ageFactor: number): string => {
+  return (total * (sinclair * ageFactor)).toFixed(2);
+};
+
+export const clamp = (val: number, min: number, max: number): number => Math.min(Math.max(val, min), max);
